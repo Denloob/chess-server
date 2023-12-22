6283eba2fad42c5793b248c3d783c9a223f64253
Denloob <den@denloob.uk>
2023-12-22 23:14:11 +0200

    feat: add point arithmetic/logic member functions

--- MagChess/Files/Point.cpp
+++ MagChess/Files/Point.cpp
@@ -13,3 +13,26 @@ Point &Point::operator+=(const Point &other)
     this->y += other.y;
     return *this;
 }
+
+Point Point::operator-() const
+{
+    return {-x, -y};
+}
+
+Point Point::operator-(const Point &other) const
+{
+    Point copy = *this;
+    copy -= other;
+
+    return copy;
+}
+
+Point &Point::operator-=(const Point &other)
+{
+    return *this += (-other);
+}
+
+bool Point::operator==(const Point &other) const
+{
+    return this->x == other.x && this->y == other.y;
+}
