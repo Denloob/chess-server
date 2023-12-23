9016e0440933531f0d91401118ccdab645183b36
Denloob <den@denloob.uk>
2023-12-23 12:26:51 +0200

    feat: implement pawn moves

--- MagChess/Files/Pawn.cpp
+++ MagChess/Files/Pawn.cpp
@@ -1,8 +1,63 @@
-#include "Pawn.h"
 #include "Board.h"
+#include "Pawn.h"
 #include "Utils.h"
 
-void Pawn::add_attacks_to(Attacks& attacks) const
+void Pawn::add_attacks_to(Attacks &attacks) const
 {
-    Utils::add_above_row_attacks_to(attacks, *this);
+    using namespace Utils::Direction;
+
+    Utils::add_attacks_at_position_to(
+        {
+            UP + RIGHT,
+            UP + LEFT,
+        },
+        attacks, *this);
+}
+
+bool Pawn::can_move_to(const Point &pos) const
+{
+    return is_move_to_safe(pos) &&
+           (can_advance_to(pos) || can_double_dvance_to(pos) ||
+            can_attack(pos));
+}
+
+bool Pawn::can_double_dvance_to(const Point &pos) const
+{
+    using namespace Utils::Direction;
+
+    const Board &board = *this->board();
+
+    Point movement_direction = pos - this->pos();
+    bool path_to_target_empty = (board.at(this->pos() + UP) == nullptr &&
+                                 board.at(this->pos() + UP + UP) == nullptr);
+
+    return movement_direction == UP + UP && !has_moved() &&
+           path_to_target_empty;
+}
+
+bool Pawn::can_advance_to(const Point &pos) const
+{
+    using namespace Utils::Direction;
+
+    const Board &board = *this->board();
+
+    const Piece *target = board.at(pos);
+    bool target_empty = (target == nullptr);
+    Point movement_direction = pos - this->pos();
+
+    return movement_direction == UP && target_empty;
+}
+
+bool Pawn::can_attack(const Point &pos) const
+{
+    const Board &board = *this->board();
+
+    const Piece *target = board.at(pos);
+    bool target_empty = (target == nullptr);
+
+    Attacks attacks{};
+    add_attacks_to(attacks);
+
+    return attacks.at(pos.x).at(pos.y) && !target_empty &&
+           target->color() != this->color();
 }
--- MagChess/Files/Pawn.h
+++ MagChess/Files/Pawn.h
@@ -4,8 +4,15 @@
 
 class Pawn : public RestrictedPiece
 {
-    using RestrictedPiece::can_move_to;
+  public:
     using RestrictedPiece::RestrictedPiece;
 
-    void add_attacks_to(Attacks& attacks) const override;
+    void add_attacks_to(Attacks &attacks) const override;
+    bool can_move_to(const Point &pos) const override;
+
+  private:
+    bool can_advance_to(const Point &pos) const;
+    bool can_double_dvance_to(const Point &pos) const;
+    bool can_attack(const Point &pos) const;
+    bool can_en_passant(const Point &pos) const;
 };
