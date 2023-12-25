74eb5b940d6fb2c24ba005602cefd60c72727c99
Denloob <den@denloob.uk>
2023-12-25 15:56:26 +0200

    style: spacing

--- MagChess/Files/MagshimimUI.cpp
+++ MagChess/Files/MagshimimUI.cpp
@@ -1,7 +1,5 @@
 #include "MagshimimUi.h"
 
-
-
 MagshimimUI::MagshimimUI(Board* board)
 {
     this->_pipe.connect();
@@ -14,18 +12,17 @@ MagshimimUI::~MagshimimUI()
 
 void MagshimimUI::submit(Client::MoveResult result)
 {
-    std::string send_msg{ static_cast<char> (result) };
+    std::string send_msg{static_cast<char>(result)};
     send_msg += '\0';
     this->_pipe.sendMessageToGraphics(&send_msg[0]);
 }
 
 Client::Move MagshimimUI::receive()
 {
-    std::string server_string = this->_pipe.getMessageFromGraphics(); 
     std::string first_half = server_string.substr(0, 1);
+    std::string server_string = this->_pipe.getMessageFromGraphics();
     std::string second_half = server_string.substr(2, 3);
     Point source(to_point(first_half));
     Point destination(to_point(second_half));
-    return Client::Move{ source ,destination };
+    return Client::Move{source, destination};
 }
-
