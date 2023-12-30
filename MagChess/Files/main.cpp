ffeb2962527cb951c927f19a5ced18ce8a3b274b
Denloob <den@denloob.uk>
2023-12-30 08:22:41 +0200

    feat: add bot main

--- MagChess/Files/main.cpp
+++ MagChess/Files/main.cpp
@@ -1,16 +1,34 @@
-#include "Board.h"
-#include "Client.h"
-#include "MagshimimUi.h"
+#include "Bot.h"
+#include <dpp/dpp.h>
 #include <string>
 
-int main()
+std::string get_bot_token()
 {
-
-    Board b;
-    MagshimimUI ui{&b};
-
-    while (true)
+    if (const char *env_token = std::getenv("BOT_TOKEN"))
     {
-        ui.submit(b.do_move(ui.receive()));
+        return env_token;
     }
+
+    std::string token;
+    std::cin >> token;
+
+    return token;
+}
+
+bool is_flag_present(int argc, char **argv, std::string_view flag)
+{
+    return std::any_of(argv + 1, argv + argc,
+                       [&flag](std::string_view arg) { return arg == flag; });
+}
+
+int main(int argc, char **argv)
+{
+    Bot bot{get_bot_token()};
+
+    if (is_flag_present(argc, argv, "--recreate-commands"))
+    {
+        bot.on_ready([&bot](const dpp::ready_t &) { bot.register_commands(); });
+    }
+
+    bot.start();
 }
